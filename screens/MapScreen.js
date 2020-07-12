61ef5f5ca7f3d84820545d36276fd9db6ff0bcf0
Chinedum Ukejianya <cukejianya@gmail.com>
2020-07-11 23:27:55 -0400

    Update mapscreen

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -8,16 +8,26 @@ import {
     View,
 } from 'react-native';
 import {Permissions} from 'expo';
-import MapView, { Callout, Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import moment from 'moment-timezone'
 
+import {hp, wp} from '../utils/scale';
 import mapStyle from '../constants/MapStyle'
 import { MapCarousel, SearchBar } from '../components/index.js'
 
 import data from '../config/data'
 export default class MapScreen extends React.Component {
   static navigationOptions =  ({navigation}) => ({
-    title: "Explore",
+    headerTitle: (
+      <View>
+        <Text 
+          style={styles.headerTitle} 
+          onPress={navigation.getParam('toggleZoom')}
+        >
+          Explore
+        </Text>
+      </View>
+    ),
     headerStyle: {
       backgroundColor: '#1A0554',
       borderColor: "#000",
@@ -40,25 +50,48 @@ export default class MapScreen extends React.Component {
     let move = data();
     this.state = {
       isLoading: false,
+      zoomIn: false,
       dataSource: move,
     }
   }
 
   componentDidMount(){
     //this.getMoves();
+    this.props.navigation.setParams({toggleZoom: this._toggleZoom})
     this._getLocationAsync()
+  }
+
+  _toggleZoom = () => {
+    return // For now. this.map.getCamera doesnt work when using expo client. Remove during production.
+    console.log(this.map.animateToRegion);
+    let camera = this.map.getCamera();
+    let {latitude, longitude} = camera.center;
+    if (this.state.zoomIn) {
+      this._moveToRegion(latitude, longitude, 0.01);
+    } else {
+      this._moveToRegion(latitude, longitude, 0.03);
+    }
+
+    this.setState({zoomIn: !this.state.zoomIn});
+  }
+
+  _moveToRegion = (lat, lng, delta) => {
+    let region = {
+      latitude: lat,
+      longitude: lng,
+      latitudeDelta: delta,
+      longitudeDelta: delta,
+    }
+
+    this.map.animateToRegion(region);
+    
+    return;
   }
 
   _moveToEvent = (moveIdx) => {
     let {lat, lng} = this.state.dataSource[moveIdx];
-    let region = {
-      latitude: lat - 0.0025,
-      longitude: lng,
-      latitudeDelta: 0.01,
-      longitudeDelta: 0.01,
-    }
-    this.map.animateToRegion(region);
-
+    this._moveToRegion(lat, lng, 0.01);
+    this.setState({zoomIn: true});
     return;
   }
 
@@ -124,23 +157,34 @@ export default class MapScreen extends React.Component {
       )
     });
 
-    let map = () => (
-      <MapView style={styles.map}
-        ref={ref => {this.map = ref; }}
-        initialRegion={{
-            latitude: this.state.dataSource[0].lat - 0.05,
-            longitude: this.state.dataSource[0].lng,
-            latitudeDelta: 0.3,
-            longitudeDelta: 0.3,
-        }}
-        provider={PROVIDER_GOOGLE}
-        customMapStyle={mapStyle}
-        showUserLocation
-      >
-        {markers()}
-      </MapView>
-    );
-
+    let map = () => {
+      let padding = {
+        top: 0,
+        right: 0,
+        left: 10,
+        bottom: hp(40),
+      }
+      return (
+        <MapView style={styles.map}
+          ref={ref => {this.map = ref; }}
+          initialRegion={{
+              latitude: this.state.dataSource[0].lat - 0.05,
+              longitude: this.state.dataSource[0].lng,
+              latitudeDelta: 0.3,
+              longitudeDelta: 0.3,
+          }}
+          provider={PROVIDER_GOOGLE}
+          customMapStyle={mapStyle}
+          showsUserLocation={true}
+          showsMyLocationButton={true}
+          showsCompass={true}
+          mapPadding={padding}
+        >
+          {markers()}
+        </MapView>
+      );
+    }
+    
     let searchBar = () => (
       <SearchBar
         onPress={() => this.props.navigation.navigate('List')}
@@ -149,16 +193,17 @@ export default class MapScreen extends React.Component {
       /> 
     );
 
-    let carousel = () => (
-      <MapCarousel
-        style={styles.carousel}
-        ref={ref => {this.carousel = ref; }}
-        data={this.state.dataSource}
-        onSnapToItem={this._moveToEvent}
-        onItemPress={this._openEvent}
-      />
-    );
-
+    let carousel = () => {
+      return (
+        <MapCarousel
+          style={styles.carousel}
+          ref={ref => {this.carousel = ref; }}
+          data={this.state.dataSource}
+          onSnapToItem={this._moveToEvent}
+          onItemPress={this._openEvent}
+        />
+      )
+    }
     return (
       <View style={{flex: 1}}>
         {statusBar()}
@@ -173,6 +218,10 @@ export default class MapScreen extends React.Component {
 //this.props.navigation.navigate('App');
 
 const styles = StyleSheet.create({
+  headerTitle: {
+    color: "#fff",
+    fontSize: 20,
+  },
   map: {
     position: 'absolute',
     top: 0,
@@ -189,3 +238,5 @@ const styles = StyleSheet.create({
     bottom: 20,
   }
 });
+
+
